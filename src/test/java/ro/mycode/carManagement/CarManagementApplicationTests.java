ff57e12fa82b512def4111543232f70c6ad077d8
Balanescu Raul <balanescuraul@gmail.com>
2023-01-27 20:17:07 +0200

    add spring

---
+++ src/test/java/ro/mycode/carManagement/CarManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.mycode.carManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
